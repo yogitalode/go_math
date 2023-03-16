741534e96fc4e6a0d62b2f51f40bfa3dabb4aff6
yogitalode <yogitalode.dlt@gmail.com>
2023-03-16 10:03:11 +0530

    zero commit is not allowed

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,5 +1,11 @@
 package geometry
 
-func Cubevolume(n int) int {
-	return n * n * n
+import "errors"
+
+func Cubevolume(n int) (int, error) {
+	if n != 0 {
+		return n * n * n, nil
+	} else {
+		return 0, errors.New("Zero length edge is not allowed")
+	}
 }
